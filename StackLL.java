465a97ad620bd870127ffc8d4ffef055af3646b3
prathamm2506 <fs20if005@gmail.com>
2024-12-21 11:58:01 +0530

    Stack

--- StackLL.java
+++ StackLL.java
@@ -60,12 +60,23 @@
 import java.util.*;
 
 public class StackLL{
+    public static void pushAtBottom(int data, Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(0);
         s.push(1);
         s.push(2);
         s.push(3);
+
+        pushAtBottom(4, s);
 
         while(!s.isEmpty()){
             System.out.println(s.peek());
